5d5b2e23f33b7ca9d35ce38c3c987030ca78ac85
xshen <xiaohe_shen@brown.edu>
2024-10-09 07:09:59 -0500

    added validation

--- optimizeFit_abcdnn.py
+++ optimizeFit_abcdnn.py
@@ -1,9 +1,11 @@
+# python3 optimizeFit_abcdnn.py -f skewNorm_cubic -c case14 --doValidation
 import os
 import numpy as np
 from ROOT import *
 from argparse import ArgumentParser
 
 gStyle.SetOptFit(1)
+#gROOT.SetBatch(True) # suppress histogram display
 
 # define the fit function
 # f(x) = 2 pdf(x) * cdf(a x)
@@ -13,13 +15,10 @@ gStyle.SetOptFit(1)
 parser = ArgumentParser()
 parser.add_argument("-f", "--fitType" , default="skewNorm_cubic")
 parser.add_argument("-c", "--case"    , default="case14")
-#parser.add_argument("--productionMode", action="store_true")
+parser.add_argument("--doValidation", action = "store_true")
 
 fitType = parser.parse_args().fitType
 case = parser.parse_args().case
-
-#if args.productionMode:
-#    print("Not in production mode. Optimzation run...\n")
 
 binlo = 400
 binhi = 2500
@@ -39,7 +38,7 @@ elif fitType=="skewNorm_cubic":
     fitFunc = "[3] * (2/[1]) * ( TMath::Exp(-((x-[2])/[1])*((x-[2])/[1])/2) / TMath::Sqrt(2*TMath::Pi()) ) * ( (1 + TMath::Erf([0]*((x-[2])/[1])/TMath::Sqrt(2)) ) / 2 ) + [4] + [5] * x + [6] * x * x + [7] * x * x * x"
     nparams = 8
     fit = TF1("fitFunc", fitFunc, 400, 2500, nparams)
-    fit.SetParameters(5, 400, 500, 50, 0.00001, 0.000001, 0.0000001)
+    fit.SetParameters(5, 400, 500, 50, 0.00001, 0.000001, 0.00000001)
 elif fitType=="landau":
     fitFunc = "TMath::Landau(x, [0], [1])"
     nparams = 2
@@ -68,81 +67,64 @@ else:
 #skewFit.SetParameters(5, 400, 500, 50, 0.00001, 0.000001, 0.00000001, 0.0000000001)
 #skewFit.SetParameters(5, 500, 500, 50, 0.00001, 0.000001, 0.000000001)
 
+def fit_and_plot(hist, plotname):
+    c = TCanvas("")
+    latex = TLatex()
+    latex.SetNDC()
+    
+    hist.Scale(1/hist.Integral())
+    hist.Fit(fit, "E")
+    hist.Draw()
+        
+    chi2ndof = fit.GetChisquare() / fit.GetNDF()
+    latex.DrawText(0.6, 0.2, f'chi2/ndof = {round(chi2ndof,2)}')
+    
+    c.SaveAs(plotname)
+    print(f'Saved plot to {plotname}.')
 
+    
 histFile = TFile.Open(f'hists_ABCDnn_{case}_{binlo}to{binhi}_{bins}.root', "READ")
 
 # store parameters in dictionaries
 params = {"tgt":{},
           "pre":{}
           }
-lastbin = {"tgt":{},
-           "pre":{}
-           }
 
-for region in ["A", "B", "C", "D", "X", "Y"]:
-    params["tgt"][region] = {}
-    params["pre"][region] = {}
-#    for i in range(nparams): # the number of params
-#        params["tgt"][region][str(i)] = {}
-#        params["pre"][region][str(i)] = {}
+if parser.parse_args().doValidation:
+    hist_abcdnn = histFile.Get(f'Bprime_mass_ABCDnn_val')
+    fit_and_plot(hist_abcdnn, f'{plotDir}/fit_ABCDnn_validation.png')
+else:
+    for region in ["A", "B", "C", "D", "X", "Y"]:
+        params["tgt"][region] = {}
+        params["pre"][region] = {}
 
-# fit
-# hist range: [500, 2500]
-for region in ["A", "B", "C", "D", "X", "Y"]:
-    for htype in ["tgt", "pre"]:
-        c = TCanvas("")
-        latex = TLatex()
-        latex.SetNDC()
-        if htype == "tgt":
-            hist = histFile.Get(f'Bprime_mass_tgt_{region}_{case}') - histFile.Get(f'Bprime_mass_mnr_{region}_{case}')
-        elif htype == "pre":
-            hist = histFile.Get(f'Bprime_mass_pre_{region}_{case}')
-        else:
-            print(f'Undefined histogram type {htype}. Check for typo.')
+    # fit
+    # hist range: [400, 2500]
+    for region in ["A", "B", "C", "D", "X", "Y"]:
+        for htype in ["tgt", "pre"]:
+            if htype == "tgt":
+                hist = histFile.Get(f'Bprime_mass_tgt_{region}_{case}') - histFile.Get(f'Bprime_mass_mnr_{region}_{case}')
+            elif htype == "pre":
+                hist = histFile.Get(f'Bprime_mass_pre_{region}_{case}')
+            else:
+                print(f'Undefined histogram type {htype}. Check for typo.')
 
+            fit_and_plot(hist, f'{plotDir}/fit_{htype}_{region}.png')
             
-        # last bin is not fitted. get bin content
-        lastbin[htype][region] = hist.GetBinContent(len(hist))
-        
-        hist.Scale(1/hist.Integral())
+            for i in range(nparams):
+                params[htype][region][str(i)] = fit.GetParameter(i)
 
-        hist.Fit(fit, "E")
-        hist.Draw()
-        
-        chi2ndof = fit.GetChisquare() / fit.GetNDF()
-        latex.DrawText(0.6, 0.2, f'chi2/ndof = {round(chi2ndof,2)}')
-        
-        c.SaveAs(f'{plotDir}/fit_{htype}_{region}.png')
-        print(f'Saved plot to {plotDir}/fit_{htype}_{region}.png')
+    histFile.Close()
 
-        for i in range(nparams):
-            params[htype][region][str(i)] = fit.GetParameter(i)
+    # compare
+    params_uncert = {}
+    for i in ["0", "1", "2", "3", "4", "5", "6"]:
+        uncert = 0
+        for region in ["A", "B", "C"]: # excluded X, Y. Y p6 oddly different.
+            uncert += abs((params["pre"][region][i]-params["tgt"][region][i])/params["tgt"][region][i])
 
-histFile.Close()
-
-# compare
-params_uncert = {}
-for i in ["0", "1", "2", "3", "4", "5", "6"]:
-    uncert = 0
-    for region in ["A", "B", "C"]: # excluded X, Y. Y p6 oddly different.
-        uncert += abs((params["pre"][region][i]-params["tgt"][region][i])/params["tgt"][region][i])
-    
-    params_uncert[i] = uncert/3
-    print(f'Avg deviation in param{i}: {100*uncert/3}%')
-
-lastbin_uncert = 0
-for region in ["A", "B", "C"]:
-    lastbin_uncert = abs(lastbin["pre"][region]-lastbin["tgt"][region])/lastbin["tgt"][region]
-lastbin_uncert = lastbin_uncert/3
-
-# fill last bin separately
+        params_uncert[i] = uncert/3
+        print(f'Avg deviation in param{i}: {100*uncert/3}%')
 
 
-# shift
-#for i in ["0", "1", "2", "3", "4", "5", "6"]:
-    
-#    params["pre"]["D"][i] * (1+params_uncert[i])
-#    params["pre"]["D"][i] * (1-params_uncert[i])
-    #print(f'Resulting param{i}_up is {params["pre"]["D"]"')
 
-# fill last bin separately
